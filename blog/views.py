1cf12670a8e827894a9405c2a01185703ddcf82f
MajidHashemi <majid2022hashemi@gmail.com>
2025-09-14 13:57:45 +0330

    update view to display Hello Django + Docker + Nginx 👋

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,9 @@
-from django.shortcuts import render
-
-# # Create your views here.
 
 
-# from django.http import HttpResponse
+# blog/views.py
+from django.http import HttpResponse
 
-# def home(request):
-#     return HttpResponse("<h1>Hello Django + Docker + Nginx 👋</h1>")
+def home(request):
+    return HttpResponse("<h1>Hello Django + Docker + Nginx 👋</h1>")
 
+    
