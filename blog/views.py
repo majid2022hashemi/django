db2f39fefbe2f5392dcf05f0ced17e5a25891478
MajidHashemi <majid2022hashemi@gmail.com>
2025-09-20 23:35:22 +0330

    Use SEO-friendly URLs for blog posts
    - Updated post_detail URL to include year, month, day, and slug.
    - Modified post_detail view to match new URL parameters.
    - Updated get_absolute_url() in Post model to generate canonical URLs using publish date and slug.

--- blog/views.py
+++ blog/views.py
@@ -19,14 +19,17 @@ def post_list(request, debug=False):
         return HttpResponse(f"Error: {str(e)}")
 
 
-def post_detail(request, id):
-    """
-    Show details of a single published post.
-    """
+def post_detail(request, year, month, day, post):
     post = get_object_or_404(
         Post,
-        id=id,
-        status=Post.Status.PUBLISHED
+        status=Post.Status.PUBLISHED,
+        slug=post,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day
     )
-    return render(request, 'blog/post/detail.html', {'post': post})
-
+    return render(
+        request,
+        'blog/post/detail.html',
+        {'post': post}
+    )
