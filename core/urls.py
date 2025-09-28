e6db05aaf13227458f486c1a3f73fdf618d2313a
MajidHashemi <majid2022hashemi@gmail.com>
2025-09-28 22:00:13 +0330

    feat(blog): add sitemap support for published posts
    - enabled  and  in settings
    - set  in base settings
    - created  with  class
    - updated  to expose sitemap.xml endpoint

--- core/urls.py
+++ core/urls.py
@@ -2,15 +2,25 @@
 # core/urls.py
 
 from django.contrib import admin
-from django.urls import path, include
+from django.contrib.sitemaps.views import sitemap
+from django.urls import include, path
+from blog.sitemaps import PostSitemap
+
+
+sitemaps = {
+    'posts': PostSitemap,
+}
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-
-    # مسیر HTML وبلاگ
-    path("blog/", include("blog.urls", namespace="blog")),
-
-    # API عمومی پروژه
+    path('admin/', admin.site.urls),
+    path('blog/', include('blog.urls', namespace='blog')),
+    path(
+        'sitemap.xml',
+        sitemap,
+        {'sitemaps': sitemaps},
+        name='django.contrib.sitemaps.views.sitemap'
+    ),
+     # API عمومی پروژه
     path("api/", include("api.urls", namespace="api")),
 
     # API مربوط به بلاگ
