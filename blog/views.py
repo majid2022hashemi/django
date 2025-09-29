4dec794875a3b041739feaacfb8b6cab1d937f58
MajidHashemi <majid2022hashemi@gmail.com>
2025-09-29 15:14:08 +0330

    feat(search): add trigram similarity search and create pg_trgm extension

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,4 @@
-
-# blog/views.py
+from django.contrib.postgres.search import TrigramSimilarity
 from django.core.mail import send_mail
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.db.models import Count
@@ -7,14 +6,9 @@ from django.shortcuts import get_object_or_404, render
 from django.views.decorators.http import require_POST
 from django.views.generic import ListView
 from taggit.models import Tag
+
 from .forms import CommentForm, EmailPostForm, SearchForm
 from .models import Post
-from django.contrib.postgres.search import (
-    SearchVector,
-    SearchQuery,
-    SearchRank,
-)
-
 
 
 def post_list(request, tag_slug=None):
@@ -166,33 +160,29 @@ def post_comment(request, post_id):
     )
 
 
-
-
 def post_search(request):
     form = SearchForm()
     query = None
     results = []
-    if "query" in request.GET:
+
+    if 'query' in request.GET:
         form = SearchForm(request.GET)
         if form.is_valid():
-            query = form.cleaned_data["query"]
-            search_vector = SearchVector("title", "body")
-            search_query = SearchQuery(query)
+            query = form.cleaned_data['query']
             results = (
                 Post.published.annotate(
-                    search=search_vector,
-                    rank=SearchRank(search_vector, search_query),
+                    similarity=TrigramSimilarity('title', query),
                 )
-                .filter(search=search_query)
-                .order_by("-rank")
+                .filter(similarity__gt=0.1)
+                .order_by('-similarity')
             )
+
     return render(
         request,
-        "blog/post/search.html",
+        'blog/post/search.html',
         {
-            "form": form,
-            "query": query,
-            "results": results,
+            'form': form,
+            'query': query,
+            'results': results
         },
     )
-
