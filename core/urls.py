099923d6926dee383542a667942133b9d8686e96
MajidHashemi <majid2022hashemi@gmail.com>
2025-10-13 11:01:45 +0330

    feat(account): implement authentication system
    - Add login form with custom Form class
    - Create account static assets (CSS)
    - Implement login and base templates
    - Set up account URLs and view updates
    - Integrate authentication into core URLs
    
    Files added:
    - account/forms.py
    - account/static/css/base.css
    - account/templates/account/login.html
    - account/templates/base.html
    - account/urls.py
    - base.html
    - login.html
    
    Files modified:
    - account/views.py
    - core/urls.py

--- core/urls.py
+++ core/urls.py
@@ -12,6 +12,7 @@ sitemaps = {
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('account/', include('account.urls', namespace='account')),
     path('blog/', include('blog.urls', namespace='blog')),
     path(
         'sitemap.xml',
