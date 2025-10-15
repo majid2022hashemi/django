1fa1aed65e2a07465cc1e0513308e003f5e57f7b
MajidHashemi <majid2022hashemi@gmail.com>
2025-10-15 09:13:28 +0330

    using django built-in authenticationn views

--- core/settings/base.py
+++ core/settings/base.py
@@ -92,3 +92,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'dashboard'
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
