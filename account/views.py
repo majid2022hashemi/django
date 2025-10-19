5cfc61f06992597cd70be11e89d38da3cecea806
MajidHashemi <majid2022hashemi@gmail.com>
2025-10-19 13:51:29 +0330

    feat(account): add user registration, update login template with register link

--- account/views.py
+++ account/views.py
@@ -1,8 +1,10 @@
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
 from django.shortcuts import render
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
 from django.contrib.auth.decorators import login_required
+
+
 
 def user_login(request):
     if request.method == 'POST':
@@ -36,3 +38,18 @@ def dashboard(request):
         {'section': 'dashboard'}
     )
 
+
+
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)  # هنوز ذخیره نکن
+            new_user.set_password(user_form.cleaned_data['password'])  # هش کردن رمز عبور
+            new_user.save()  # ذخیره در دیتابیس
+            return render(request, 'account/register_done.html', {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+    
+    return render(request, 'account/register.html', {'user_form': user_form})
